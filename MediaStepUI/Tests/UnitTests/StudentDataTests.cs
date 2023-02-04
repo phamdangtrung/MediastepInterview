1e3dc2836865b86a048ecac62abadf09e60b24c0
phamdangtrung <55026963+phamdangtrung@users.noreply.github.com>
2023-02-04 10:14:32 +0700

    Add test files

---
+++ MediaStepUI/Tests/UnitTests/StudentDataTests.cs
@@ -0,0 +1,36 @@
+﻿namespace MediaStepUI.Tests.UnitTests;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class StudentDataTests
+{
+    private readonly IStudentData _studentData;
+
+    public StudentDataTests(IStudentData studentData)
+    {
+        _studentData = studentData;
+    }
+
+    [TestMethod]
+    public async Task GetStudentsAndSort_ShouldReturnADictionary()
+    {
+        var expected = typeof(IDictionary<string, IEnumerable<Student>>);
+
+        var studentList = await _studentData.GetStudentsAndSort();
+
+        var result = studentList.GetType();
+        Assert.IsInstanceOfType(result, expected); 
+    }
+
+    [TestMethod]
+    public async Task GetStudentsAndSort_ShouldReturnADictionaryWith30Items()
+    {
+        int expected = 30;
+
+        var studentList = await _studentData.GetStudentsAndSort();
+
+        var result = studentList.Count();
+        Assert.AreEqual(expected, result);
+    }
+}
